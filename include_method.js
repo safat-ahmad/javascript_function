b99814c5aeeb999c8be614ec8b2cc8e0109572ef
Safat Ahmad <safatahmadkhan65@gmail.com>
2025-01-17 22:34:12 +0530

    array includes method

---
+++ include_method.js
@@ -0,0 +1,6 @@
+function checkElement(arr, element){
+    return arr.includes(element);
+
+}
+let myArray = ["banana", "apple", "mango","grapes", "pineapple"];
+console.log(checkElement(myArray, 'banana'));
