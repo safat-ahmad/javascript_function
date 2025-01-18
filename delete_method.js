a63c00a87be96516ee88aee1161086fcefef85a6
Safat Ahmad <safatahmadkhan65@gmail.com>
2025-01-18 12:11:11 +0530

    Javascript array sort method

---
+++ delete_method.js
@@ -0,0 +1,6 @@
+function arraydeleteMethod(arr){
+    for (let i = 0; i<=arr.length-1; i++){
+
+    }
+}
+let 
---
+++ sort_method.js
@@ -0,0 +1,6 @@
+function sorthArrayofInteger(a, b){
+    return a - b;
+}
+let arr = [1,4,2,7,5,8,4];
+arr.sort(sorthArrayofInteger)
+console.log(arr);
