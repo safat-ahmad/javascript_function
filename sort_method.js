50357add0de8b58376929de146562da37ee487a5
Safat Ahmad <safatahmadkhan65@gmail.com>
2025-01-18 12:51:03 +0530

    Array.from method in javascript

--- sort_method.js
+++ sort_method.js
@@ -1,6 +1,9 @@
 function sorthArrayofInteger(a, b){
-    return a - b;
+    let assending = a-b
+
+    return assending;
 }
 let arr = [1,4,2,7,5,8,4];
 arr.sort(sorthArrayofInteger)
+
 console.log(arr);
---
+++ str_to_array.js
@@ -0,0 +1,11 @@
+function strToArr(){
+
+    let arr = Array.from(name)
+    return arr;
+    
+}
+
+let name = "Hello";
+let result = strToArr(name);
+console.log(result);
+
