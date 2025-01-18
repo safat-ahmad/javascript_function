50f4ffa88daca4e5b5a23bf940d7cdcb1c032244
Safat Ahmad <safatahmadkhan65@gmail.com>
2025-01-18 14:40:32 +0530

    remove midd character of a string

---
+++ remove_miidlestr_ofarr.js
@@ -0,0 +1,14 @@
+function remove(str){
+    if (str.length == 0){
+        return str;
+    }
+    let middle = Math.floor(str.length / 2);
+    if(str.length % 2 ==0){
+        return  str.slice(0,middle -1) + str.slice(middle +1);
+    }else{
+        return str.slice(0,middle) + str.slice(middle +1);
+    }
+}
+let myStr = "abcde";
+let result = remove(myStr);
+console.log(result);
