66f8e2e740da2c7c01c278a90da5a87b2f6b48bc
Safat Ahmad <safatahmadkhan65@gmail.com>
2025-01-18 13:04:42 +0530

    convert integer to array & integer to string of a number

---
+++ integer_to_array.js
@@ -0,0 +1,17 @@
+function intToarr(int){
+
+    let arr = String(num).split("").map(Number);
+    return arr;
+}
+let num = 123456789;
+let result = intToarr(num)
+console.log(result);
+
+
+function intTostr(int){
+    let array = String(nmbr).split("");
+    return array;
+}
+let nmbr = 987654321;
+let results = intTostr(nmbr);
+console.log(results);
