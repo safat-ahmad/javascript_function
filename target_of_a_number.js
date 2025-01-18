7dfa118c77ae680b98b41cd1329a953a0f271855
Safat Ahmad <safatahmadkhan65@gmail.com>
2025-01-18 18:36:41 +0530

    target of a number of K

---
+++ target_of_a_number.js
@@ -0,0 +1,14 @@
+function targetOfaNumber(arr){
+    let k = 20;
+    let number = [];
+    for(let i = 0; i <= arr.length-1; i++){
+        if(arr[i] !== k){
+            number.push(arr[i])
+        }
+
+    }
+    return number;
+}
+let myArray = [10, 15, 20, 25, 20, 40, 50, 20, 30, 20];
+let result = targetOfaNumber(myArray);
+console.log(result);
