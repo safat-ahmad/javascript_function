f7fd0abe89016482043199c59d6c8b6cdb5e0689
Safat Ahmad <safatahmadkhan65@gmail.com>
2025-01-19 16:19:19 +0530

    indexOf and lastindexOf array Method

---
+++ indexOf_method.js
@@ -0,0 +1,21 @@
+function indexOfAndLastindexOf(){
+    let a = ["apple","mango","banana","pineapple", "banana", "banana","grapes"];
+    let b = a.indexOf("banana");
+    console.log(b);
+
+
+    let c = ["apple","mango","banana","pineapple", "banana", "banana","grapes"];
+    let d = c.indexOf("banana", 3)
+    console.log(d);
+
+    let e = ["apple","mango","banana","pineapple", "banana", "banana","grapes"];
+     let f = e.lastIndexOf("banana")
+     console.log(f);
+     
+
+    
+
+    
+
+}
+indexOfAndLastindexOf()
