505cee842d62084e18a72a709c92d2874edff997
Safat Ahmad <safatahmadkhan65@gmail.com>
2025-01-21 21:25:54 +0530

    Rest argument in Javascript function

--- javascript_function.js
+++ javascript_function.js
@@ -8,3 +8,15 @@ function restParameter(...args){
 let number = restParameter(10,20,30,40,50,60,70)
 
 console.log(number);
+
+
+function restParametr(...arg){
+    let totalSum=0;
+    for(let z of arg)
+        totalSum = z + totalSum
+        return totalSum;
+    
+}
+let sumSumber = restParametr(10,20,30,40);
+console.log(sumSumber);
+
