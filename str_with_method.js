3c3a73bac6ab5a11135c55f01e1657dabebcc84f
Safat Ahmad <safatahmadkhan65@gmail.com>
2025-01-21 21:35:17 +0530

    String includes startwith and endwidth method in JavaScript

---
+++ str_with_method.js
@@ -0,0 +1,12 @@
+let word= "Hello World How are you and where are you living"
+console.log(word.startsWith("world"));
+
+
+let words = "Hello World How are you and where are you living";
+console.log(words.includes("you"));
+
+
+
+let letter =  "Hello World How are you and where are you living";
+console.log(letter.endsWith("living"));
+
