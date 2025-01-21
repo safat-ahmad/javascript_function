37aa0de703ac23fc90cdd14c8f45933a1a15e4a1
Safat Ahmad <safatahmadkhan65@gmail.com>
2025-01-21 20:18:16 +0530

    JavaScript getter and setter

--- classes.js
+++ classes.js
@@ -8,3 +8,16 @@ class car {
 
 const car1 = new car("mahindra", "Tata",2000  );
 console.log(car1.name + "\n" + car1.brand + "\n"+ car1.year);
+
+
+let cars = {
+    name:"thor",
+    brand:"Tata",
+    model:200,
+    set lang(value) {
+        this.name = value;
+      }
+}
+
+cars.lang="Happy"
+console.log(cars.name);
