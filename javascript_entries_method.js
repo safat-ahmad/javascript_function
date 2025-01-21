b0fb334d1e33838533102343f8cc080333b0b431
Safat Ahmad <safatahmadkhan65@gmail.com>
2025-01-21 21:47:20 +0530

    Array from Method in JAvascript

---
+++ javascript_entries_method.js
@@ -0,0 +1,11 @@
+// let cars = ["tata","ferrari","mahindra","nano"];
+// let c = cars.entries();
+
+// for(let x of c)
+
+//     console.log( x );
+
+
+let letters = "ABCDEF"
+let mayarr = Array.from(letters)
+console.log(mayarr);
