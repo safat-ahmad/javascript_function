9444b8c6ab4ddcc09467be130e9168cfe9f63820
Safat Ahmad <safatahmadkhan65@gmail.com>
2025-01-22 18:39:07 +0530

    synchronous and asynchronous Javascript

---
+++ sync_async_javascript.js
@@ -0,0 +1,24 @@
+// synchronous javascript
+let x = [1,2,3,4,5];
+let y = "Hello";
+let z = 123;
+let a = false;
+console.log(x);
+console.log(y);
+console.log(z);
+console.log(a);
+
+
+// asynchronous javascript
+
+
+console.log("hello");
+setTimeout(() =>{
+console.log("World");
+
+}, 2000);
+console.log("Safat");
+
+
+
+
