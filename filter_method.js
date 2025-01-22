6b145184d760822f2bdc1541642838fa4e74504f
Safat Ahmad <safatahmadkhan65@gmail.com>
2025-01-22 13:22:25 +0530

    JavaScripth Filter Method

--- filter_method.js
+++ filter_method.js
@@ -16,3 +16,38 @@ function myOdd(value){
 }
 console.log(odd);
 
+
+
+
+let cars = ["volvo", "mahindra","thor","bugati","scorpio"];
+let bigLength = cars.filter(value => value.length >=5)
+console.log(bigLength);
+
+
+
+let person = [
+    {name:"safat",
+    age:20
+    },
+    {
+    name:"mukhtar",
+    age:18
+    },
+    {
+        name:"wakar",
+        age:15
+    },
+    {
+        name:"hello",
+        age:10
+    },
+    {
+        name:"zzzz",
+        age:50
+    }
+
+];
+
+let adultAge = person.filter(name => name.age < 20)
+console.log(adultAge);
+
