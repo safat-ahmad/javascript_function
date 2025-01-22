30ac6fb35887a7cb89f81f7cf2fd37992a620d29
Safat Ahmad <safatahmadkhan65@gmail.com>
2025-01-22 13:05:18 +0530

    JavaScript Map method

--- map_method.js
+++ map_method.js
@@ -1,10 +1,10 @@
-// let arr = [1,2,3,4,5,6,7,8,9];
-// let myArr = arr.map(mapFunction);
+let arr = [1,2,3,4,5,6,7,8,9];
+let myArr = arr.map(mapFunction);
 
-// function mapFunction(value, index){
-//     return value +  index
-// }
-// console.log(myArr);
+function mapFunction(value, index){
+    return value +  index
+}
+console.log(myArr);
 
 
 
