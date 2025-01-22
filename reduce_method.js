cebbaf30262c3463b9ab465e5a14772516ad4f69
Safat Ahmad <safatahmadkhan65@gmail.com>
2025-01-22 13:37:19 +0530

    Javascript reduce method

---
+++ reduce_method.js
@@ -0,0 +1,14 @@
+
+let number = [1,2,3,4,5];
+let sum = number.reduce((value, curretValue) => value + curretValue, 0);
+console.log(sum);
+
+
+
+
+let num = [1,2,3,4,5];
+let square = num.reduce((accumulator,currentValue) => accumulator * currentValue,1)
+console.log(square);
+
+
+
