e765145d569d54b983d519f673ee922e7b8b6035
Safat Ahmad <safatahmadkhan65@gmail.com>
2025-01-22 13:04:41 +0530

    JAvaScript map method

---
+++ arr_keys_method.js
@@ -0,0 +1,5 @@
+let name =["hello","how","are","you"];
+let keys = name.keys();
+let text = "";
+
+for()
---
+++ map_method.js
@@ -0,0 +1,15 @@
+// let arr = [1,2,3,4,5,6,7,8,9];
+// let myArr = arr.map(mapFunction);
+
+// function mapFunction(value, index){
+//     return value +  index
+// }
+// console.log(myArr);
+
+
+
+
+let numbers = [1,2,3,4,5,6]
+let squire = numbers.map(value => value ** value)
+    console.log(squire);
+
